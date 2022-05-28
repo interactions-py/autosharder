bacd1caa708146fdbc7d3e6c0a13f5269b6285ab
EdVraz <88881326+EdVraz@users.noreply.github.com>
2022-05-28 12:01:12 +0200

    chore: save version before making a new release
    bruh

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as ld:
 
 setup(
     name="interactions-autosharder",
-    version="1.0.0",
+    version="1.0.1",
     description="Autosharder for interactions.py",
     long_description=long_description,
     long_description_content_type="text/markdown",
